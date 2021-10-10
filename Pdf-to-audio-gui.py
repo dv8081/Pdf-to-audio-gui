fe7377a7c9054e584a1932b70fe88a2a0df2275d
Divyanshu Verma <63949148+dv8081@users.noreply.github.com>
2021-10-10 09:18:06 +0530

    Update Pdf-to-audio-gui.py

--- Pdf-to-audio-gui.py
+++ Pdf-to-audio-gui.py
@@ -1,3 +1,16 @@
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # importing the modules
 import PyPDF2
 import pyttsx3
@@ -5,12 +18,32 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
+#def page_select():
 
+
+def banner():
+    print("PPPPPPPPPPPPPPPPP   ")
+    print("PPPPP          PPPPPP   ")
+    print("PPPPP         PPPPPP   ")
+    print("PPPPP       PPPPPP   ")
+    print("PPPPP PPPPPPPPPPP  ")
+    print("PPPPP   ")
+    print("PPPPP   ")
+    print("PPPPP   ")
+    print("PPPPP   ")
+    print("PPPPP   ")
+    print("PPP   ")
+banner()
+
+k=int(input("Enter Page no:"))
 # create the root window
 root = tk.Tk()
 root.title('Select a Pdf file to Play in Audio')
 root.resizable(True,True )
 root.geometry('500x500')
+#open(pathofpdf, 'rb')
+
+Select_page=(k)
 
 
 def select_file():
@@ -23,10 +56,10 @@ def select_file():
 
     filename = fd.askopenfilename(
         title='Open a file',
-        initialdir='/',
+        initialdir='~/Documents/java',
         filetypes=filetypes)
     # path of the PDF file
-    pathofpdf=("Enter the path of filkr")
+    pathofpdf=("Enter the path of file")
     path = filename
     engine = pyttsx3.init()
     rate = engine.getProperty('rate')
@@ -35,9 +68,10 @@ def select_file():
     # creating a PdfFileReader object
     pdfReader = PyPDF2.PdfFileReader(path)
 
+
     # the page with which you want to start
-    # this will read the page of 25th page.
-    from_page = pdfReader.getPage(24)
+   
+    from_page = pdfReader.getPage(k)
 
     # extracting the text from the PDF
     text = from_page.extractText()
@@ -62,6 +96,11 @@ open_button = ttk.Button(
 )
 
 open_button.pack(expand=True)
+#quit button
+Quit_button=tk.Button(
+    root,
+    text="Click Here To Quit",
+    command=root.quit,).pack()
 
 
 # run the application
